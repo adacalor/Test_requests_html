e31a1de7bfa16c21ae11949bab6b96164c001789
[adacalor] <[adacalor@gmail.com]>
2020-07-30 10:04:05 +0200

    :white_check_mark: First test html requests

---
+++ request_html_test.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 29 17:26:43 2020
+
+@author: Collabcap
+"""
+
+from requests_html import HTMLSession
+session = HTMLSession()
+
+r = session.get('https://python.org/')
+
+
+r.html.render()
+
+r = session.get('https://www.collaborationcapital.org/mentions-legales', timeout=10)
+
+print( r.html.render() )
